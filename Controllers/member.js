be45923ea10ed3f55da239c5d0a1c3ec1bb6d910
kivaghani <kartikvaghani@rapideaseinnovations.com>
2024-12-30 10:53:08 +0530

    Complete all dashboard features backend

--- Controllers/member.js
+++ Controllers/member.js
@@ -146,29 +146,179 @@ exports.monthlyMember = async (req, res) => {
   }
 };
 
-
-
 exports.expiringWithin3Days = async (req, res) => {
-    try {
-        const today = new Date();
-        const nextThreeDays = new Date();
-        nextThreeDays.setDate(today.getDate() + 3);
+  try {
+    const today = new Date();
+    const nextThreeDays = new Date();
+    nextThreeDays.setDate(today.getDate() + 3);
 
-        const member = await Member.find({ gym : req.gym._id,
-            nextBillDate : {
-                $gte : today,
-                $lte : nextThreeDays
-            }
+    const member = await Member.find({
+      gym: req.gym._id,
+      nextBillDate: {
+        $gte: today,
+        $lte: nextThreeDays,
+      },
+    });
+
+    res.status(200).json({
+      message: member.length
+        ? "Fetched Members Successfully"
+        : "No Such Member is Expiring within 3 days",
+      members: member,
+      totalMembers: member.length,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.expiringWithin4to7Days = async (req, res) => {
+  try {
+    const today = new Date();
+    const next4Days = new Date();
+    next4Days.setDate(today.getDate() + 4);
+
+    const next7Days = new Date();
+    next7Days.setDate(today.getDate() + 7);
+
+    const member = await Member.find({
+      gym: req.gym._id,
+      nextBillDate: {
+        $gte: next4Days,
+        $lte: next7Days,
+      },
+    });
+
+    res.status(200).json({
+      message: member.length
+        ? "Fetched Members Successfully"
+        : "No Such Member is Expiring within 4-7 days",
+      members: member,
+      totalMembers: member.length,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.expiredMember = async (req, res) => {
+  try {
+    const today = new Date();
+    const member = await Member.find({
+      gym: req.gym._id,
+      status: "Active",
+      nextBillDate: {
+        $lt: today,
+      },
+    });
+    res.status(200).json({
+      message: member.length
+        ? "Fetched Members Successfully"
+        : "No Such Member has been Expired",
+      members: member,
+      totalMembers: member.length,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.inActiveMember = async (req, res) => {
+  try {
+    const member = await Member.find({ gym: req.gym._id, status: "Pending" });
+
+    res.status(200).json({
+      message: member.length
+        ? "Fetched Members Successfully"
+        : "No Such Member has been Pending",
+      members: member,
+      totalMembers: member.length,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.getMemberDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const member = await Member.findById({ _id: id, gym: req.gym._id });
+    if (!member) {
+      return res.status(404).json({
+        error: "No Such Member",
+      });
+    }
+    res.status(200).json({
+      message: "Member Date fetched",
+      member: member,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.changeStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+    const member = await Member.findByIdAndUpdate({
+      _id: id,
+      gym: req.gym._id,
+    });
+    if (!member) {
+      return res.status(404).json({
+        error: "No Such Member",
+      });
+    }
+
+    member.status = status;
+    await member.save();
+    res.status(200).json({
+      message: "Status Changed Successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.updateMemberPlan = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { membership } = req.body;
+    const memberShip = await Membership.findOne({
+      gym: req.gym._id,
+      _id: membership,
+    });
+    if (memberShip) {
+      let getMonth = memberShip.months;
+      let today = new Date();
+      let nextBillDate = addMonthToDate(getMonth, today);
+      const member = await Member.findOne({ gym: req.gym._id, _id: id });
+      if (!member) {
+        return res.status(404).json({
+          error: "No Such Member are there",
         });
-
-        res.status(200).json({
-            message : member.length? "Fetched Members Successfully" : "No Such Member is Expiring within 3 days",
-            members : member,
-            totalMembers : member.length
-        })
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Server error" });
       }
-    };
+      member.nextBillDate = nextBillDate;
+      member.lastPayment = today;
+
+      await member.save();
+      res.status(200).json({
+        message: "Member Renewed Successfully",member
+      })
+    }else{
+      return res.status(404).json({
+        error: "No such Membership are there",
+      })
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
--- Routes/member.js
+++ Routes/member.js
@@ -9,6 +9,12 @@ router.post('/register-member', auth, MemberController.registerMember)
 router.get('/searched-members', auth, MemberController.searchMember)
 router.get('/monthly-member', auth, MemberController.monthlyMember)
 router.get('/within-3-days-expiring', auth, MemberController.expiringWithin3Days)
+router.get('/within-4-7-expiring', auth, MemberController.expiringWithin4to7Days)
+router.get('/expired-member', auth, MemberController.expiredMember)
+router.get('/inactive-member', auth, MemberController.inActiveMember)
 
+router.get('/get-member/:id', auth, MemberController.getMemberDetails)
+router.put('/change-status/:id', auth, MemberController.changeStatus)
+router.put('/update-member-plan/:id', auth, MemberController.updateMemberPlan)
 
 module.exports = router;
