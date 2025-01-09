36402e4a59b80b29e80d892abbca60425a874e2b
kivaghani <kartikvaghani@rapideaseinnovations.com>
2025-01-09 13:34:45 +0530

    changes

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const cors = require('cors')
+const cors = require("cors");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT  
+const CLIENT_URL = process.env.CLIENT_URL
 
-app.use(cors({
-  origin : 'http://localhost:5173',
-  credentials : true
-}))
-
+app.use(
+  cors({
+    origin: CLIENT_URL, 
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
+
 require("./DBConn/conn");
 
 const GymRoutes = require("./Routes/gym");
 const MembershipRoutes = require("./Routes/membership");
-const MemberRoutes = require('./Routes/member')
+const MemberRoutes = require("./Routes/member");
 
 app.use("/auth", GymRoutes);
 app.use("/plans", MembershipRoutes);
 app.use("/members", MemberRoutes);
 
 app.get("/", (req, res) => {
-  res.send({ message: "Congratulation Tour server is running" });
+  res.send({ message: "Congratulations! Your server is running successfully." });
 });
 
 app.listen(PORT, () => {
