589a1b2972fe258618aae99249da06a1a6fcdc88
kivaghani <kartikvaghani@rapideaseinnovations.com>
2025-01-09 12:40:52 +0530

    connection

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-
 mongoose
-  .connect("mongodb://localhost:27017/gymbackend")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => {
     console.log(err);
